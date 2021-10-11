95e31a3e7aa19cc7aa321e0e33587d9527a103b3
Andrey <55235312+andrew133@users.noreply.github.com>
2021-10-11 15:18:31 +0300

    04

--- 01.js
+++ 01.js
@@ -2,3 +2,12 @@ var remainder;
 globalThis;
 var product = 8 * 0;
 var difference = 45 - 0;
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a = a / 12;
+b = b / 4;
+c = c / 11;
